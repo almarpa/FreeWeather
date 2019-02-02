0fa6b360b18a1b6ca00a1ef5c54576cdb1fcbe54
almarpa <alex_marco7@outlook.es>
2019-02-02 14:36:21 +0100

    Primera peticion HTTP  a la API : CON ÉXITO

--- app/src/main/java/upv/tfg/freeweather/MainActivity.java
+++ app/src/main/java/upv/tfg/freeweather/MainActivity.java
@@ -1,7 +1,29 @@
 package upv.tfg.freeweather;
 
+import android.app.DownloadManager;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +32,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View view) {
+        getFriendScores task = new getFriendScores();
+            task.execute();
+    }
+
+    private void rellenarDatos(GsonGeneral gs) {
+        TextView tvDatos =  findViewById(R.id.tvDatos);
+        tvDatos.setText(gs.datos);
+    }
+
+
+    //Obt
+    public class getFriendScores extends AsyncTask<Void, Void, Void> {
+
+        private GsonGeneral gs;
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                //Peticion GET
+                URL url = new URL("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/46250?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbGV4X21hcmNvN0BvdXRsb29rLmVzIiwianRpIjoiM2YxYmQyZDAtYTdjNy00MjNhLTljMDktYWFiMmQ4OTdlN2RmIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1NDU3Nzk0NTIsInVzZXJJZCI6IjNmMWJkMmQwLWE3YzctNDIzYS05YzA5LWFhYjJkODk3ZTdkZiIsInJvbGUiOiIifQ.rf0HtYhn5FEGYUhZn_y2wnel8GrpuPKuQj2JZ35GG7Q");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                gs = gson.fromJson(reader, GsonGeneral.class);
+
+                connection.disconnect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void params) {
+            rellenarDatos(gs);
+        }
     }
 }
